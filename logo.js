556279a770098af83d55d4ec1d70cc10d5b78f15
MuhammadAliAli123 <thunder_storm2612@yahoo.com>
2018-08-09 12:22:24 +0500

    hamdard responsive
    hamdard responsive

---
+++ logo.js
@@ -0,0 +1,4 @@
+document.write('<div class="container">'
+ +  '<a class="navbar-brand"><img src="img/original1.PNG" alt=""></a>'
+ +  '</div>'
+)
---
+++ news.js
@@ -0,0 +1,11 @@
+document.write(  '<div id="breaking-news-container">'
+ +               ' <div id="breaking-news-colour" class="slideup animated">'
+ +                ' </div>'
+ +                '  <span style="color: red;" class="breaking-news-title delay-animated slidein">'
++                '     ** News **'
++              '     </span> '
++                '   <a style="color: white" class="breaking-news-headline delay-animated2 fadein marquee">'
++                 'Hamdard Islamicus for the month of June and July is now available.'
++                 '</a> ' 
++               '</div> <br>'
+)
